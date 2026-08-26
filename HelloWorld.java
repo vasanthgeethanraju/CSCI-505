594ecefb746f5709c59118796e27e6f8b29049d4
Vasanth Geethan Raju <vasanthgeethan.raju@gmail.com>
2026-08-26 16:35:53 -0700

    CSCI-505 1.4 assignment code

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+// This is a simple Java program that prints "Hello, World!" to the console.
+// It defines a class named `HelloWorld` with a `main` method, which is the entry point of the program.
+// When executed, it will display the message on the screen.
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
